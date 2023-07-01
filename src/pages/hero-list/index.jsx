3cf45584d1df09e524bf15ad4e6774da2ca0d3ad
Yasolar <ssousayasmin@gmail.com>
2023-07-01 03:12:48 -0300

    layout styled component em hero list

--- src/pages/hero-list/index.jsx
+++ src/pages/hero-list/index.jsx
@@ -1,12 +1,25 @@
 import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { HerosContext } from '../../store/contexts'
-import ProfileSpider  from '../../assets/ProfileSpider.png'
+import ProfileSpider from '../../assets/ProfileSpider.png'
+import {
+    HeaderContainer,
+    BackButton,
+    BackIcon,
+    TitleContainer,
+    HerosContainer,
+    InfosContainer,
+    HeroContainer,
+    HeroImg,
+    NameContainer,
+    DetailsContainer,
+    HeroDiv,
+} from './style'
 
 export default function HeroList() {
-    const {isLoading, setIsLoading} = useContext(HerosContext)
-    const {infos, setInfos} = useContext(HerosContext)
-    const {setName} = useContext(HerosContext)
+    const { isLoading, setIsLoading } = useContext(HerosContext)
+    const { infos, setInfos } = useContext(HerosContext)
+    const { name, setName } = useContext(HerosContext)
     const navigate = useNavigate()
 
     const toHome = () => {
@@ -23,67 +36,83 @@ export default function HeroList() {
         } else {
             img = ProfileSpider
         }
-        navigate('/HeroDetails', {state: { img, name, desc }})
+        navigate('/HeroDetails', { state: { img, name, desc } })
     }
 
     return (
-        <div style={{marginInline: '20px'}}>
-            <button onClick={() => toHome()}>VOLTAR</button>
+        <>
+            <HeaderContainer>
+                <BackButton onClick={() => toHome()}>
+                    <BackIcon>&lt;</BackIcon>
+                    Voltar
+                </BackButton>
+                <TitleContainer>
+                    Veja os resultados da sua pesquisa sobre "{name}"
+                </TitleContainer>
+            </HeaderContainer>
             {isLoading ?
-                <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
-                    <div style={{cursor: 'pointer', position: 'relative', marginLeft: '40px',marginBottom: '30px', border: 'solid', borderRadius: '5px', borderWidth: '2px', borderColor:'grey', display: 'flex', alignItems: 'center', flexDirection: 'row', height: '120px', width: '250px', paddingLeft: '20px'}}>
-                        <div style={{ backgroundColor: 'lightgray', borderRadius: '100%', width: '80px', height: '80px', marginRight: '30px' }} />
-                        <div style={{ flex: 1 }}>
-                            <p style={{fontSize: '15px', marginRight: '20px'}}>
+                <HerosContainer>
+                    <HeroContainer>
+                        <InfosContainer>
+                            <HeroDiv />
+                            <NameContainer>
                                 -
-                            </p>
-                            <button style={{cursor: 'pointer', borderRadius: '50%', width: '35px',height:'35px', position: 'absolute', top: '50%', right: '-18px', transform: 'translateY(-50%)'}}>
-                                +
-                            </button>
-                        </div>
-                    </div>
-
-                    <div style={{cursor: 'pointer', position: 'relative', marginLeft: '40px',marginBottom: '30px', border: 'solid', borderRadius: '5px', borderWidth: '2px', borderColor:'grey', display: 'flex', alignItems: 'center', flexDirection: 'row', height: '120px', width: '250px', paddingLeft: '20px'}}>
-                        <div style={{ backgroundColor: 'lightgray', borderRadius: '100%', width: '80px', height: '80px', marginRight: '30px' }} />
-                        <div style={{ flex: 1 }}>
-                            <p style={{fontSize: '15px', marginRight: '20px'}}>
-                                -
-                            </p>
-                            <button style={{cursor: 'pointer', borderRadius: '50%', width: '35px',height:'35px', position: 'absolute', top: '50%', right: '-18px', transform: 'translateY(-50%)'}}>
-                                +
-                            </button>
-                        </div>
-                    </div>
-
-                    <div style={{cursor: 'pointer', position: 'relative', marginLeft: '40px',marginBottom: '30px', border: 'solid', borderRadius: '5px', borderWidth: '2px', borderColor:'grey', display: 'flex', alignItems: 'center', flexDirection: 'row', height: '120px', width: '250px', paddingLeft: '20px'}}>
-                        <div style={{ backgroundColor: 'lightgray', borderRadius: '100%', width: '80px', height: '80px', marginRight: '30px' }} />
-                        <div style={{ flex: 1 }}>
-                            <p style={{fontSize: '15px', marginRight: '20px'}}>
-                                -
-                            </p>
-                            <button style={{cursor: 'pointer', borderRadius: '50%', width: '35px',height:'35px', position: 'absolute', top: '50%', right: '-18px', transform: 'translateY(-50%)'}}>
-                                +
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            :
-                <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
-                    {infos.map(info =>
-                        <div key={info.id} onClick={() => toHeroDetails(info.thumbnail, info.name, info.description)} style={{cursor: 'pointer', position: 'relative', marginLeft: '40px',marginBottom: '30px', border: 'solid', borderRadius: '5px', borderWidth: '2px', borderColor:'grey', display: 'flex', alignItems: 'center', flexDirection: 'row', height: '120px', width: '250px', paddingLeft: '20px'}}>
-                            <img style={{ backgroundColor: 'lightgray', borderRadius: '100%', width: '80px', height: '80px', marginRight: '30px' }} src={info.thumbnail && !info.thumbnail.path.includes('image_not_available') ? `${info.thumbnail.path}.${info.thumbnail.extension}` : ProfileSpider} alt="" />
-                            <div style={{ flex: 1 }}>
-                                <p style={{fontSize: '15px', marginRight: '20px'}}>
-                                    {info.name ? info.name : "-"}
-                                </p>
-                                <button style={{cursor: 'pointer', borderRadius: '50%', width: '35px',height:'35px', position: 'absolute', top: '50%', right: '-18px', transform: 'translateY(-50%)'}} onClick={() => toHeroDetails(info.thumbnail, info.name, info.description)}>
-                                    +
-                                </button>
+                            </NameContainer>
+                        </InfosContainer>
+                        <DetailsContainer>
+                            <div>
+                                Ver detalhes
                             </div>
-                        </div>
+                        </DetailsContainer>
+                    </HeroContainer>
+
+                    <HeroContainer>
+                        <InfosContainer>
+                            <HeroDiv />
+                            <NameContainer>
+                                -
+                            </NameContainer>
+                        </InfosContainer>
+                        <DetailsContainer>
+                            <div>
+                                Ver detalhes
+                            </div>
+                        </DetailsContainer>
+                    </HeroContainer>
+
+                    <HeroContainer>
+                        <InfosContainer>
+                            <HeroDiv />
+                            <NameContainer>
+                                -
+                            </NameContainer>
+                        </InfosContainer>
+                        <DetailsContainer>
+                            <div>
+                                Ver detalhes
+                            </div>
+                        </DetailsContainer>
+                    </HeroContainer>
+                </HerosContainer>
+                :
+                <HerosContainer>
+                    {infos.map(info =>
+                        <HeroContainer key={info.id}>
+                            <InfosContainer>
+                                <HeroImg src={info.thumbnail && !info.thumbnail.path.includes('image_not_available') ? `${info.thumbnail.path}.${info.thumbnail.extension}` : ProfileSpider} alt="" />
+                                <NameContainer>
+                                    {info.name ? info.name : "-"}
+                                </NameContainer>
+                            </InfosContainer>
+                            <DetailsContainer onClick={() => toHeroDetails(info.thumbnail, info.name, info.description)}>
+                                <div>
+                                    Ver detalhes
+                                </div>
+                            </DetailsContainer>
+                        </HeroContainer>
                     )}
-                </div>
+                </HerosContainer>
             }
-        </div>
+        </>
     )
 }
